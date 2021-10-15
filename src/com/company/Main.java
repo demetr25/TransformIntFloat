1e89abde82fdcadf6f2bf0dbea7f766ba29dfe00
Admin <dmitrijdundakov@gmail.com>
2021-10-15 22:16:04 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 2_147_483_647;
+        float f = i;
+        System.out.println("int->float     "+"i"+" "+"="+" "+i+" "+"->"+" "+"f"+" "
+        +"="+" "+f);
+    }
+}
